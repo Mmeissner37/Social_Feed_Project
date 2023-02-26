fb32ec94cd1544b23fb4eee9608fd4d0ed24a36d
Michaela Meissner <m.p.meissner85@gmail.com>
2023-02-26 12:09:00 -0500

    Finished CreatePostForm component and created handleSubmit function for Submit Button

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div>
       <NavBar />
-      <CreatePostForm />
+      <CreatePostForm parentPosts={posts}/>
 
     </div>
   );
--- src/Components/CreatePostForm/CreatePostForm.jsx
+++ src/Components/CreatePostForm/CreatePostForm.jsx
@@ -1,11 +1,27 @@
+import React, { useState } from 'react';
+
+
 
 const CreatePostForm = (props) => {
+
+    const [posterName, setName] = useState('');
+    const [postBody, setBody] = useState('');
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        let newPost = {
+            name: posterName,
+            body: postBody
+        };
+    }
+
+
     return ( 
-        <form>
+        <form onSubmit={handleSubmit}>
             <label for="name">Name: </label><br></br>
-            <input type="text" id="posterName"></input><br></br>
+            <input type="text" value={posterName} onChange={(event) => setName(event.target.value)} /><br></br>
             <label for="body">Post: </label><br></br>
-            <input type="text" id="postBody"></input>
+            <input type="text" value={postBody} onChange={(event) => setBody(event.target.value)} />
             <button type="submit">Create</button>
         </form>
      );
